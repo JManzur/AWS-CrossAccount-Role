9f6d87c4003fd61533cd2f039e8da5a5b654254d
jmanzur <jmanzurst@gmail.com>
2022-01-30 11:49:37 -0500

    Adding Comments

--- Terraform/CrossAccountRole.tf
+++ Terraform/CrossAccountRole.tf
@@ -1,3 +1,4 @@
+# Establish trust relationships with the provided account.
 resource "aws_iam_role" "ControlTowerCrossAccountRole" {
   name = "ControlTowerCrossAccountRole"
 
@@ -16,6 +17,7 @@ resource "aws_iam_role" "ControlTowerCrossAccountRole" {
   )
 }
 
+# Allow Administrator Access on all resources.
 resource "aws_iam_role_policy" "ControlTowerCrossAccountPolicy" {
   name = "ControlTowerCrossAccountPolicy"
   role = aws_iam_role.ControlTowerCrossAccountRole.id
