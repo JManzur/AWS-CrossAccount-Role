60cf65d19a984130663cf5d287e5983d497d196b
jmanzur <jmanzurst@gmail.com>
2022-01-31 20:14:19 -0500

    Fix Typo in Terraform Template

--- Terraform/CrossAccountRole.tf
+++ Terraform/CrossAccountRole.tf
@@ -1,6 +1,6 @@
 # Establish trust relationships with the provided account.
-resource "aws_iam_role" "ControlTowerCrossAccountRole" {
-  name = "ControlTowerCrossAccountRole"
+resource "aws_iam_role" "AWSControlTowerExecution" {
+  name = "AWSControlTowerExecution"
 
   assume_role_policy = jsonencode({
     "Version" : "2012-10-17",
@@ -18,9 +18,9 @@ resource "aws_iam_role" "ControlTowerCrossAccountRole" {
 }
 
 # Allow Administrator Access on all resources.
-resource "aws_iam_role_policy" "ControlTowerCrossAccountPolicy" {
-  name = "ControlTowerCrossAccountPolicy"
-  role = aws_iam_role.ControlTowerCrossAccountRole.id
+resource "aws_iam_role_policy" "AWSControlTowerExecutionPolicy" {
+  name = "AWSControlTowerExecutionPolicy"
+  role = aws_iam_role.AWSControlTowerExecution.id
 
   policy = jsonencode({
     "Version" : "2012-10-17",
